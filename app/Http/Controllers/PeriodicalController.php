5f814692c86277b12a62aec9e7e5780e8a32fb29
Raveniis <128274869+Raveniis@users.noreply.github.com>
2024-03-26 16:48:01 +0800

    simple read update
    read for projects, articles, periodicals and books for OPAC

--- app/Http/Controllers/PeriodicalController.php
+++ app/Http/Controllers/PeriodicalController.php
@@ -6,5 +6,54 @@ use Illuminate\Http\Request;
 
 class PeriodicalController extends Controller
 {
+<<<<<<< Updated upstream
     //
+=======
+    public function getPeriodicals() {
+        return Periodical::all();
+    }
+
+    public function getByType($type) {
+        return Periodical::where('material_type', $type)->get();
+    }
+
+    public function getPeriodical($id) {
+        return Periodical::find($id);
+    }
+
+    public function add(Request $request) {
+        $model = new Periodical();
+        $model->fill($request->all());
+        $model->save();
+
+        return response()->json($model, 200);
+    }
+
+    public function update(Request $request, $id) {
+        $model = Periodical::find($id);
+        $model->update($request->all());
+        $model->save();
+
+        return response()->json($model, 200);
+    }
+
+    public function delete($id) {
+        $model = Periodical::find($id);
+        $model->delete();
+
+        return response()->json('Record Deleted', 200);
+    }
+
+    public function opacGetMagazines(){
+        return Periodical::where('material_type', 'magazine')->select('title', 'copyright', 'author', 'image_location')->paginate(25);
+    }
+
+    public function opacGetjournals(){
+        return Periodical::where('material_type', 'journal')->select('title', 'copyright', 'author', 'image_location')->paginate(25);
+    }
+
+    public function opacGetNewspapers(){
+        return Periodical::where('material_type', 'newspaper')->select('title', 'copyright', 'author', 'image_location')->paginate(25);
+    }
+>>>>>>> Stashed changes
 }
--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,5 +6,62 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+<<<<<<< Updated upstream
     //
+=======
+    public function getProjects() {
+        return Project::all();
+    }
+
+    public function getByType($type) {
+        return Project::where('type', $type)->get();
+    }
+
+    public function getProject($id) {
+        return Project::find($id);
+    }
+
+    public function add(Request $request) {
+        $model = new Project();
+        $model->fill($request->all());
+        $model->save();
+
+        return response()->json($model, 200);
+    }
+
+    public function update(Request $request, $id) {
+        $model = Project::find($id);
+        $model->update($request->all());
+        $model->save();
+
+        return response()->json($model, 200);
+    }
+
+    public function delete($id) {
+        $model = Project::find($id);
+        $model->delete();
+
+        return response()->json('Record Deleted', 200);
+    }
+
+    public function opacGetThesis(){
+        return Project::where('type', 'thesis')->select('title', 'author', 'image_location')->paginate(25);
+    }
+
+    public function opacGetCapstone(){
+        return Project::where('type', 'capstone')->select('title', 'author', 'image_location')->paginate(25);
+    }
+
+    public function opacGetResearch(){
+        return Project::where('type', 'research')->select('title', 'author', 'image_location')->paginate(25);
+    }
+
+    public function opacGetDissertation(){
+        return Project::where('type', 'dissertation')->select('title', 'author', 'image_location')->paginate(25);
+    }
+
+    public function opacGetFeasibility(){
+        return Project::where('type', 'feasibility study')->select('title', 'author', 'image_location')->paginate(25);
+    }
+>>>>>>> Stashed changes
 }
