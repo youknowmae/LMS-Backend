12fab64cda74a1d86fcbcba89d1286c80c9dc8ef
severusraj <rangelojam@gmail.com>
2024-05-09 07:04:33 +0800

    changing the routes by following the instruction on github but not really sure if this works out, go back on previous commit if this don't worked out.

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,48 @@
 <?php
-
-use App\Models\Book;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LockerController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MaterialController;
 
-// default view
-Route::get('/', function (Request $request) {
-    return response()->json(['Response' => 'Library Information Management System back end is up and running']);
+// Default view
+Route::get('/', function () {
+return response()->json(['Response' => 'Library Information Management System backend is up and running']);
 });
 
-Route::middleware(['auth', 'check.access:Circulation'])->group(function () {
-    // Circulation related routes here
+// Authentication routes
+Route::post('/login/{subsystem}', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+// Material routes
+Route::middleware(['auth'])->group(function () {
+Route::get('/books', [MaterialController::class, 'getAllBooks']);
+Route::get('/periodicals', [MaterialController::class, 'getAllPeriodicals']);
+Route::get('/articles', [MaterialController::class, 'getAllArticles']);
+Route::get('/projects', [MaterialController::class, 'getAllProjects']);
+
+Route::get('/book/id/{id}', [MaterialController::class, 'getBookById']);
+Route::get('/periodical/id/{id}', [MaterialController::class, 'getPeriodicalById']);
+Route::get('/article/id/{id}', [MaterialController::class, 'getArticleById']);
+Route::get('/project/id/{id}', [MaterialController::class, 'getProjectById']);
+
+Route::get('/book/image/{id}', [MaterialController::class, 'getBookImage']);
+Route::get('/periodical/image/{id}', [MaterialController::class, 'getPeriodicalImage']);
+Route::get('/project/image/{id}', [MaterialController::class, 'getProjectImage']);
+
+Route::get('/periodicals/type/{type}', [MaterialController::class, 'getPeriodicalsByType']);
+Route::get('/projects/type/{type}', [MaterialController::class, 'getProjectsByType']);
+
+Route::post('/books/process', [MaterialController::class, 'addBook']);
+Route::post('/periodicals/process', [MaterialController::class, 'addPeriodical']);
+Route::post('/articles/process', [MaterialController::class, 'addArticle']);
+Route::post('/projects/process', [MaterialController::class, 'addProject']);
+
+Route::post('/books/process/{id}', [MaterialController::class, 'updateBook']);
+Route::post('/periodicals/process/{id}', [MaterialController::class, 'updatePeriodical']);
+Route::post('/articles/process/{id}', [MaterialController::class, 'updateArticle']);
+Route::post('/projects/process/{id}', [MaterialController::class, 'updateProject']);
+
+Route::delete('/books/process/{id}', [MaterialController::class, 'deleteBook']);
+Route::delete('/periodicals/process/{id}', [MaterialController::class, 'deletePeriodical']);
+Route::delete('/articles/process/{id}', [MaterialController::class, 'deleteArticle']);
+Route::delete('/projects/process/{id}', [MaterialController::class, 'deleteProject']);
 });
-
-Route::resource('personnel', PersonnelController::class);
-
-Route::resource('patron-types', 'PatronTypeController');
-
-Route::resource('announcements', 'AnnouncementController');
-
-Route::post('/inventory/clear', 'InventoryController@clear')->middleware('auth');
-
-Route::get('/inventory/scan', 'InventoryController@showScanForm');
-Route::post('/inventory/scan', 'InventoryController@processScanForm');
-
-Route::get('/lockers/add', [LockerController::class, 'showAddForm'])
-    ->name('lockers.add')
-    ->middleware('authorizeToAddLockers');
-
-Route::post('/lockers/add', [LockerController::class, 'add'])
-    ->name('lockers.store')
-    ->middleware('authorizeToAddLockers');
