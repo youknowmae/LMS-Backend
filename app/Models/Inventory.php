bc78be482d934119cd99a62a89c2c0d7a3754aa6
severusraj <rangelojam@gmail.com>
2024-05-14 01:19:16 +0800

    Inventory backend done: implementation of api, crud, barcode.

---
+++ app/Models/Inventory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'barcode',
+        'accession_number',
+        'title',
+        'author',
+        'location',
+        'status',
+    ];
+
+    protected $casts = [
+        'barcode' => 'string',
+        'accession_number' => 'string',
+        'title' => 'string',
+        'author' => 'string',
+        'location' => 'string',
+        'status' => 'string',
+    ];
+}
