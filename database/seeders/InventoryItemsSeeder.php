b40ea5918833271e35659f78ea3cb3678bbb4e0e
severusraj <rangelojam@gmail.com>
2024-05-20 22:03:02 +0800

    test

---
+++ database/seeders/InventoryItemsSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Inventory;
+use Database\Factories\InventoryItemFactory;
+
+class InventoryItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        InventoryItemFactory::new()->create([
+            'accession_number' => '1001',
+            'location' => 'BEP',
+            'title' => 'Noli Me Tangere',
+            'author' => 'Jose Rizal',
+        ]);
+
+        InventoryItemFactory::new()->create([
+            'accession_number' => '1002',
+            'location' => 'IT',
+            'title' => 'El Filibusterismo',
+            'author' => 'Jose Rizal',
+        ]);
+
+        InventoryItemFactory::new()->create([
+            'accession_number' => '1003',
+            'location' => 'MED',
+            'title' => 'Noli Me Tangere',
+            'author' => 'Jose Rizal',
+        ]);
+
+        InventoryItemFactory::new()->create([
+            'accession_number' => '1004',
+            'location' => 'COMM',
+            'title' => 'Noli Me Tangere',
+            'author' => 'Jose Rizal',
+        ]);
+    }
+}
