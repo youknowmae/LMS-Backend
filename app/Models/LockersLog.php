b4063204aea65bcc4a6b697ba83923546dd19218
JobLinker <costamerokevin@gmail.com>
2024-05-25 14:25:05 +0800

    Update LockersLog.php

--- app/Models/LockersLog.php
+++ app/Models/LockersLog.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class LockersLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "locker_id",
+        "user_id",
+        "time_in",
+        "time_out"
+    ];
+
+    public function locker()
+    {
+        return $this->belongsTo(Locker::class);
+    }
+
+
+    public static function getLockersLogsWithUsers()
+    {
+        // Fetch LockersLog data with associated user information
+        $lockersLogsWithUsers = self::with('user')->get();
+
+        // Return the data
+        return $lockersLogsWithUsers;
+    }
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
